6fe26dcc67141a78b042eea41e91c67c76f70269
TBouder <tbouder@protonmail.com>
2021-03-15 15:33:16 +0100

    :lipstick: Adding meta achievement

--- components/Badges.js
+++ components/Badges.js
@@ -7,6 +7,18 @@
 
 const	badgeClassNames = 'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium mr-2';
 
+export function Meta() {
+	return (
+		<span
+		className={`${badgeClassNames} text-white`}
+		style={{
+			background: 'linear-gradient(90deg, rgba(20,184,166,0.2) 0%, rgba(139,92,246,0.2) 50%, rgba(236,72,153,0.2) 100%)',
+			color: 'rgba(139,92,246,1)'
+		}}>
+			{'Meta'}
+		</span>
+	);
+}
 export function DEFI() {
 	return (<span className={`${badgeClassNames} bg-gray-600 bg-opacity-20 text-gray-800`}>{'DEFI'}</span>);
 }
@@ -31,14 +43,21 @@ export function Protocole1Inch() {
 }
 export function ProtocoleUniswap() {
 	return (
-		<span className={`${badgeClassNames}`} style={{background: 'linear-gradient(128.17deg, rgba(189, 0, 255, 0.2) -14.78%, rgba(255, 31, 138, 0.2) 110.05%)', color: '#BD00FF'}}>
+		<span
+			className={`${badgeClassNames}`}
+			style={{
+				background: 'linear-gradient(128.17deg, rgba(189, 0, 255, 0.2) -14.78%, rgba(255, 31, 138, 0.2) 110.05%)',
+				color: '#BD00FF'
+			}}>
 			{'Uniswap'}
 		</span>
 	);
 }
 export function ProtocoleSushiswap() {
 	return (
-		<span className={`${badgeClassNames}`} style={{background: 'radial-gradient(circle at left top, rgba(37, 167, 219, 0.3), rgba(250, 82, 160, 0.3)', color: 'rgb(250, 81, 157)'}}>
+		<span
+			className={`${badgeClassNames}`}
+			style={{background: 'radial-gradient(circle at left top, rgba(37, 167, 219, 0.3), rgba(250, 82, 160, 0.3)', color: 'rgb(250, 81, 157)'}}>
 			{'Sushiswap'}
 		</span>
 	);
@@ -52,6 +71,7 @@ export function ProtocoleYearn() {
 }
 
 export function Badge({type}) {
+	if (type === 'meta') return (<Meta />);
 	if (type === 'defi') return (<DEFI />);
 	if (type === 'airdrop') return (<Airdrop />);
 	if (type === 'exploit') return (<Exploit />);
