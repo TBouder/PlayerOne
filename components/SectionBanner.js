5e0b2901787a0ba6b63e861009806b527d991700
TBouder <tbouder@protonmail.com>
2021-03-23 19:56:03 +0100

    :adhesive_bandage: Fixing small ui issue

--- components/SectionBanner.js
+++ components/SectionBanner.js
@@ -59,7 +59,7 @@ function	SectionBanner() {
 
 	return (
 		<section id={'preview'} aria-label={'preview'} className={'w-full mt-12'}>
-			<div ref={hoverRef} style={{height: 310}} className={'relative'}>
+			<div ref={hoverRef} style={{height: 310}} className={'relative z-10'}>
 				<ItemBannerUniswap
 					id={'firstBannerRef'}
 					ref={firstBannerRef}
