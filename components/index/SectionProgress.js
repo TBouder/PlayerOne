7b701015825c0a99bc2c020f2294e0f98e5e87cb
TBouder <tbouder@protonmail.com>
2021-03-25 21:12:50 +0100

    :lipstick: Zindex fix

--- components/index/SectionProgress.js
+++ components/index/SectionProgress.js
@@ -16,11 +16,11 @@ function	SectionProgress({unlocked, myAchievements}) {
 		return null;
 	}
 	return (
-		<section aria-label={'achievement-progress'} id={'achievement-progress'} className={'flex flex-col w-full'}>
+		<section aria-label={'achievement-progress'} id={'achievement-progress'} className={'flex flex-col w-full z-10'}>
 			<h3 className={'text-base text-teal-600 font-semibold tracking-wide uppercase mb-4 text-center'}>
 				{`${(unlocked/myAchievements.length*100).toFixed(2)}% completed`}
 			</h3>
-			<div className={'rounded overflow-hidden h-2 mb-0.5 bg-gray-200 flex flex-row mx-6 md:mx-12 lg:mx-16 xl:mx-24'}>
+			<div className={'rounded overflow-hidden h-2 mb-0.5 bg-gray-200 flex flex-row mx-6 md:mx-12 lg:mx-16 xl:mx-24 z-10'}>
 				<div className={'h-full progressBarColor rounded transition-all duration-700'} style={{width: `calc(100% * ${unlocked/myAchievements.length})`}} />
 			</div>
 			<p className={'text-gray-400 uppercase mt-2 text-center text-xs'}>
