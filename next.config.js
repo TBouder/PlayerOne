84a4c3238b20e61421b44bfd9d1c694d2af8dd18
TBouder <tbouder@protonmail.com>
2021-04-12 23:10:12 +0200

    :lock: Removing content security

--- next.config.js
+++ next.config.js
@@ -34,12 +34,6 @@ module.exports = withBundleAnalyzer({
 			minSize: 20000
 		}
 	},
-	async headers() {
-		return [{
-			source: '/', headers: securityHeaders,
-			source: '/:path*', headers: securityHeaders,
-		}]
-	},
 	webpack: (config, {webpack}) => {
 		// Note: we provide webpack above so you should not `require` it
 		// Perform customizations to webpack config
@@ -53,43 +47,3 @@ module.exports = withBundleAnalyzer({
 		return config
 	},
 })
-
-const	ContentSecurityPolicy = `
-	default-src 'self';
-	script-src 'self' 'unsafe-eval' 'unsafe-inline';
-	style-src 'self' 'unsafe-inline';
-	img-src 'self';
-	media-src 'self';
-	connect-src *;
-	font-src 'self' fonts.googleapis.com;
-`;
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, '')
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin'
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY'
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on'
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload'
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()'
-  }
-];
